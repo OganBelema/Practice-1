a263eaafbdbbec158c898547ef305f4222d575c0
Ogan Belema <Oganbelema@gmail.com>
2017-04-25 15:50:54 +0100

    update

--- HBDMum/app/src/main/java/com/example/ogan/hbdmum/MainActivity.java
+++ HBDMum/app/src/main/java/com/example/ogan/hbdmum/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.ogan.hbdmum;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.song);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.stop();
+        mediaPlayer.release();
     }
 }
